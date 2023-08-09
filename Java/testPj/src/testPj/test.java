28f324b1224ef288f1f0139a2cdd8dfc6ebdd310
이정화 <113874154+lsll3949@users.noreply.github.com>
2023-08-09 13:54:08 +0900

    Add files via upload

---
+++ Java/testPj/src/testPj/test.java
@@ -0,0 +1,16 @@
+package testPj;
+
+public class test {
+
+	public static void main(String[] args) {
+		int p = 3;
+		p+=2; //p=p+2
+		p*=3; //p=p*3
+		p-=2; //p=p-2
+		p%=3; //p=p%3
+		 
+		System.out.println(p); 
+		
+	}
+
+}
